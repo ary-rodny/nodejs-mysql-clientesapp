2def5eaf4a88740ae3469bbf9556a30b1d93d752
Ary Elivert <fojforever7@gmail.com>
2023-04-07 15:31:57 -0400

    Update mysql module to mysql2

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ const port = process.env.PORT  || 3000
 
 const DB_HOST = process.env.DB_HOST || 'localhost'
 const DB_USER = process.env.DB_USER || 'root'
-const DB_PASS = process.env.DB_PASS || 'abcd1234'
+const DB_PASS = process.env.DB_PASS || ''
 const DB_PORT = process.env.DB_PORT || 3306
 const DB_NAME = process.env.DB_NAME || 'nodemysqlcrud'
 
